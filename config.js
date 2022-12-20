2c3a5e579d1a74506262006166c74e4bdcd33764
Marjun A. Goyala <junjungoyala@gmail.com>
2022-12-20 11:31:42 +0800

    update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const constants = {
     openWeatherMap: {
         BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
-        SECRET_KEY: "caa992154ac7fbf42bfca4f5aeebf445"
+        SECRET_KEY: "41c939a35e08937abd761353dd1c5680"
     }
 }
 
